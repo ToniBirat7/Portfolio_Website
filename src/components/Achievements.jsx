c7cefdc0f5a8d579fc5ac5c206c0a64e20c4542b
Birat <gotofreelancing1@gmail.com>
2025-01-10 20:21:16 +0545

    UpdatedICTAward

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -79,6 +79,53 @@ const AwardsAchievements = () => {
           </div>
         </div>
       </div>
+
+      <div className="award-card">
+        <div className="award-header">
+          <img
+            src="../src/assets/NewPP.jpg" // Replace with the profile image URL if available
+            alt="Profile"
+            className="profile-image"
+          />
+          <div className="details">
+            <h3>Birat Gautam</h3>
+            <p>Computer Science Student</p>
+          </div>
+        </div>
+        <div className="award-content">
+          <p>ICT Award 2024: Team Baali Bigyan 🏆 </p>
+          <p>#YuwaYatra #BaaliBigyan #ICT_Top_5_Finalist_2024 </p>
+          <img
+            src="../src/assets/ICT_Top5.jpg"
+            alt="Award Achievement"
+            className="award-image"
+          />
+          <img
+            src="../src/assets/YuwaYatra_Team.jpg"
+            alt="Award Achievement"
+            className="award-image"
+          />
+          <img
+            src="../src/assets/ICT_Pic.jpg"
+            alt="Award Achievement"
+            className="award-image"
+          />
+        </div>
+        <div className="award-footer">
+          <div className="icon">
+            <i className="fas fa-thumbs-up"></i>
+            <span>400</span>
+          </div>
+          <div className="icon">
+            <i className="fas fa-comment"></i>
+            <span>20</span>
+          </div>
+          <div className="icon">
+            <i className="fas fa-share"></i>
+            <span>8</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
